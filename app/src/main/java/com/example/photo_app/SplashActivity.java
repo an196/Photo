4a27af4a18e9d9e13219f1a5b4584827c836edef
an196 <an9x0010@gmail.com>
2021-12-23 22:33:34 +0700

    get check permission

--- app/src/main/java/com/example/photo_app/SplashActivity.java
+++ app/src/main/java/com/example/photo_app/SplashActivity.java
@@ -1,9 +1,15 @@
 package com.example.photo_app;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.core.app.ActivityCompat;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class SplashActivity extends Activity {
 
@@ -12,9 +18,8 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        loadData();
+        requestPermission();
     }
-
 
     private void loadData() {
         new Handler().postDelayed(new Runnable() {
@@ -25,5 +30,39 @@ public class SplashActivity extends Activity {
                 finish();
             }
         }, 2000);
+    }
+
+    public void requestPermission() {
+        //on below line we are requesting the rea external storage permissions.
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadData();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    callRequestPermission();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void callRequestPermission() {
+        //on below line we are requesting the rea external storage permissions.
+        ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE}, 1);
     }
 }
